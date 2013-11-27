eeecd259dcba8531e2da67e879949fd2bc582577
Myke Stubbs <myke@mykestubbs.com>
2013-11-26 23:19:20 -0800

    Restructure tests to be system independent

--- TestDirectoryWalker.rb
+++ TestDirectoryWalker.rb
@@ -1,25 +1,42 @@
 #!/usr/bin/env ruby
 
 require 'minitest/autorun'
+require 'fileutils'
 require File.join(File.dirname(__FILE__), 'DirectoryWalker')
 
 class DirectoryTest < MiniTest::Unit::TestCase
 
   def setup
-    @directory = "/Users/alex/Documents/"
+    @directory = File.join(File.dirname(__FILE__), 'test')
+    Dir.mkdir(@directory)
+    Dir.chdir(@directory) do |dir|
+      ('a'..'c').each do |name|
+        File.open(name, 'w+') do |fp|
+          fp.print 'foo'
+        end
+
+        File.open("#{name}.test", 'w+') do |fp|
+          fp.print 'bar'
+        end
+      end
+    end
+  end
+
+  def teardown
+    FileUtils.rm_rf(@directory)
   end
 
   def test_calculate_documents
-    dir = DirectoryWalker.new(@directory, "")
+    dir = DirectoryWalker.new(@directory)
 
     dir.calculate
-    assert_in_delta(1778685032, dir.count, 0.01)
+    assert_in_delta(18, dir.count, 0.01)
   end
 
   def test_specific_extension
-    dir = DirectoryWalker.new(@directory, ".plist")
+    dir = DirectoryWalker.new(@directory, ".test")
 
     dir.calculate
-    assert_in_delta(180, dir.count, 0.01)
+    assert_in_delta(9, dir.count, 0.01)
   end
 end
