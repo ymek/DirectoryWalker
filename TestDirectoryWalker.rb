1a4a79933cfe1046580dd289e7de7c2cce69e9b7
Myke Stubbs <myke@mykestubbs.com>
2013-11-26 22:44:32 -0800

    Move test directory into setup method

--- TestDirectoryWalker.rb
+++ TestDirectoryWalker.rb
@@ -5,15 +5,19 @@ require File.join(File.dirname(__FILE__), 'DirectoryWalker')
 
 class DirectoryTest < MiniTest::Unit::TestCase
 
+  def setup
+    @directory = "/Users/alex/Documents/"
+  end
+
   def test_calculate_documents
-    dir = DirectoryWalker.new("/Users/alex/Documents/", "")
+    dir = DirectoryWalker.new(@directory, "")
 
     dir.calculate
     assert_in_delta(1778685032, dir.count, 0.01)
   end
 
   def test_specific_extension
-    dir = DirectoryWalker.new("/Users/alex/Desktop/", ".plist")
+    dir = DirectoryWalker.new(@directory, ".plist")
 
     dir.calculate
     assert_in_delta(180, dir.count, 0.01)
